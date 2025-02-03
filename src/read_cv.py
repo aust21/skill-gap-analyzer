06ff061c7e6bef306e4a79ecb6af8b5ae3a8d726
Austin Ngobeni <kngobeni223@gmail.com>
2025-02-03 14:59:09 +0200

    extract skills from resume

--- src/read_cv.py
+++ src/read_cv.py
@@ -1,4 +1,6 @@
-import PyPDF2, fitz, nltk
+import PyPDF2, fitz, nltk, spacy
+
+nlp = spacy.load("en_core_web_sm")
 
 def read_resume(path):
     extracted_text = ""
@@ -7,9 +9,24 @@ def read_resume(path):
         extracted_text += page.get_text("text")
     return  extracted_text
 
+# TODO: fetch common skills from an api or third party
 def common_skills():
     return ["python", "java", "sql", "machine learning", "deep learning",
               "pandas", "numpy", "nlp", "tensorflow", "pytorch", "aws",
               "docker", "flask", "django", "data analysis"]
 
-text_resume = read_resume("resume")
+def extract_skills(text, skill_list):
+    doc = nlp(text)
+    skills = set()
+
+    for token in doc:
+        # print(token.text)
+        if token.text in skill_list:
+            skills.add(token.text)
+    return skills
+
+doc = "/home/austin/Documents/docs/Austin_Ngobeni_CV.pdf"
+text_resume = read_resume(doc)
+skills = common_skills()
+skills_from_resume = extract_skills(text_resume, skills)
+print(skills_from_resume)
