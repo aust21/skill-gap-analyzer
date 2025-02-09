0a7d6ad0854578e7776a14b80f0c2307f228e027
Austin Ngobeni <kngobeni223@gmail.com>
2025-02-09 20:14:06 +0200

    start streamlit app

--- src/app.py
+++ src/app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.title('Skill gap analyser')
+st.set_page_config(page_title="My App", layout="wide")
+
+st.title("Skill gap analyser")
+
+resume_file = st.file_uploader("Attach your pdf resume", type="pdf")
+job_title = st.text_input("Enter job title")
