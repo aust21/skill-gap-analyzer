ac5cefffa3d7618f281b19a4dbe45eab6c926f52
Austin Ngobeni <kngobeni223@gmail.com>
2025-02-10 21:45:42 +0200

    create analytics page

--- src/static/js/dash.js
+++ src/static/js/dash.js
@@ -84,31 +84,4 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     },
   });
-
-  const select = document.getElementById("field");
-  let options = Array.from(select.options);
-
-  // Remove the "Select" option from sorting
-  const selectOption = options.shift();
-
-  // Sort options alphabetically by their text
-  options.sort((a, b) => a.text.localeCompare(b.text));
-
-  // Add the "Select" option back at the beginning
-  options.unshift(selectOption);
-
-  // Rebuild the select options
-  select.innerHTML = "";
-  options.forEach((option) => select.add(option));
-
-  function addOptions() {
-    const choice = document.getElementById("field").value;
-    const newFieldDiv = document.getElementById("newField");
-
-    if (choice == "Other") {
-      newFieldDiv.style.display = "block";
-    } else {
-      newFieldDiv.style.display = "none";
-    }
-  }
 });
