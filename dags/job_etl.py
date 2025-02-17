07c674409a2eb2d4811860c515c3f3e37d54bf54
Austin Ngobeni <kngobeni223@gmail.com>
2025-02-17 21:57:31 +0200

    create extract data task

--- dags/job_etl.py
+++ dags/job_etl.py
@@ -1,13 +1,17 @@
-import json, requests
+import json, requests, os
 from airflow import DAG
 from airflow.decorators import task
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.utils.dates import days_ago
+import pandas as pd
 
 default_args = {
     "owner" : "skill-gap",
     "start_date" : days_ago(1)
 }
+
+POSTGRES_CONN_ID = "postgres_default"
+API_CONN_ID = "open_meteo_api"
 
 with DAG(
     dag_id="skill_gap_pipeline",
@@ -18,4 +22,9 @@ with DAG(
 
     @task
     def extract_data():
-        pass
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(current_dir, 'resources', 'sample_data.json')
+        file = pd.read_json(file_path)
+        return file
+
+    sample_data = extract_data()
