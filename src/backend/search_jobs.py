1872951f160e4fa4061f1cd0c98eb1669e6155ea
Austin Ngobeni <kngobeni223@gmail.com>
2025-03-25 18:16:02 +0200

    fixed what I broke

--- src/backend/search_jobs.py
+++ src/backend/search_jobs.py
@@ -8,21 +8,8 @@ API_KEY = os.getenv("RAPID_API")
 API_HOST = os.getenv("RAPID_API_HOST")
 
 def search(job_title: str):
-    url = "https://linkedin-data-api.p.rapidapi.com/search-jobs"
+    url = f"https://remotive.com/api/remote-jobs?category={job_title}"
 
-    querystring = {"keywords":job_title,"locationId":"92000000","datePosted":"anyTime","sort":"mostRelevant"}
-
-    headers = {
-        "x-rapidapi-key": API_KEY,
-        "x-rapidapi-host": API_HOST
-    }
-
-    response = requests.get(url, headers=headers, params=querystring)
-
-    # print(response.json()["data"][0]["id"])
-    print(response.json())
-
-    return response.json()["data"][0]["id"]
 
 def get_descriptions(job_id: str):
     url = "https://linkedin-data-api.p.rapidapi.com/get-job-details"
